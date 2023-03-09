ae7054e61016e8efc4da9c0be59b66ed65b47258
Tianren Dong <tianrendong.dtr@gmail.com>
2023-03-09 02:38:04 +0000

    style: auth router

---
+++ server/internal/router/auth.go
@@ -0,0 +1,93 @@
+package router
+
+import (
+	"crypto/sha1"
+	"strings"
+	"time"
+
+	"github.com/fullstackatbrown/auth-infrastructure/internal/config"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-pkgz/auth"
+	"github.com/go-pkgz/auth/avatar"
+	"github.com/go-pkgz/auth/provider"
+	"github.com/go-pkgz/auth/token"
+	"golang.org/x/oauth2"
+)
+
+func DefaultAuthService() *auth.Service {
+	opts := auth.Opts{
+		SecretReader: token.SecretFunc(func(id string) (string, error) { // secret key for JWT
+			return "secret", nil
+		}),
+		TokenDuration:  time.Hour * 24 * 14, // token expires in 14 days
+		CookieDuration: time.Hour * 24 * 14, // cookie expires in 14 days
+		Issuer:         "fsab-auth",
+		DisableXSRF:    true,
+		URL:            "http://localhost:8000",
+		AvatarStore:    avatar.NewLocalFS("/tmp"),
+		ClaimsUpd: token.ClaimsUpdFunc(func(claims token.Claims) token.Claims { // modify issued token
+			if claims.User != nil {
+				// check if user is in allowed email domains
+				if len(config.Config.AllowedEmailDomains) > 0 {
+					for _, domain := range config.Config.AllowedEmailDomains {
+						if strings.HasSuffix(claims.User.Email, domain) {
+							// TODO save to db and add roles and stuff
+							return claims
+						}
+					}
+					return claims // don't save to db
+				}
+			}
+			return claims // don't save to db
+		}),
+	}
+
+	service := auth.NewService(opts)
+
+	gClient := auth.Client{
+		Cid:     config.Config.OAuth2.ClientID,
+		Csecret: config.Config.OAuth2.ClientSecret,
+	}
+
+	service.AddCustomProvider("google", gClient, provider.CustomHandlerOpt{
+		Endpoint: oauth2.Endpoint{
+			AuthURL:  "https://accounts.google.com/o/oauth2/v2/auth",
+			TokenURL: "https://oauth2.googleapis.com/token",
+		},
+		InfoURL: "https://www.googleapis.com/oauth2/v3/userinfo",
+		MapUserFn: func(data provider.UserData, _ []byte) token.User {
+			userInfo := token.User{
+				ID: "google_" + token.HashID(sha1.New(),
+					data.Value("sub")),
+				Name:    data.Value("name"),
+				Email:   data.Value("email"),
+				Picture: data.Value("picture"),
+			}
+			// fail if email is not in AllowedEmailDomains
+			if len(config.Config.AllowedEmailDomains) > 0 {
+				for _, domain := range config.Config.AllowedEmailDomains {
+					if strings.HasSuffix(userInfo.Email, domain) {
+						return userInfo
+					}
+				}
+				return token.User{}
+			}
+			return userInfo
+		},
+		Scopes: []string{"profile", "email"},
+	})
+
+	return service
+}
+
+func AuthRoutes() *chi.Mux {
+	router := chi.NewRouter()
+
+	service := DefaultAuthService()
+	authRoutes, avaRoutes := service.Handlers()
+
+	router.Mount("/", authRoutes)
+	router.Mount("/avatar", avaRoutes)
+
+	return router
+}
--- server/internal/server/server.go
+++ server/internal/server/server.go
@@ -1,23 +1,15 @@
 package server
 
 import (
-	"crypto/sha1"
 	"fmt"
 	"log"
 	"net/http"
-	"strings"
-	"time"
 
 	"github.com/fullstackatbrown/auth-infrastructure/internal/config"
-	"github.com/fullstackatbrown/auth-infrastructure/internal/handler"
+	rtr "github.com/fullstackatbrown/auth-infrastructure/internal/router"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
-	"github.com/go-pkgz/auth"
-	"github.com/go-pkgz/auth/avatar"
-	"github.com/go-pkgz/auth/provider"
-	"github.com/go-pkgz/auth/token"
 	"github.com/rs/cors"
-	"golang.org/x/oauth2"
 )
 
 func Start() {
@@ -25,84 +17,17 @@ func Start() {
 		log.Panic("Missing configuration!")
 	}
 
-	// define options
-	authOptions := auth.Opts{
-		SecretReader: token.SecretFunc(func(id string) (string, error) { // secret key for JWT
-			return "secret", nil
-		}),
-		TokenDuration:  time.Hour * 24 * 14, // token expires in 14 days
-		CookieDuration: time.Hour * 24 * 14, // cookie expires in 14 days
-		Issuer:         "fsab-auth",
-		DisableXSRF:    true,
-		URL:            "http://localhost:8000",
-		AvatarStore:    avatar.NewLocalFS("/tmp"),
-		ClaimsUpd: token.ClaimsUpdFunc(func(claims token.Claims) token.Claims { // modify issued token
-			if claims.User != nil {
-				// check if user is in allowed email domains
-				if len(config.Config.AllowedEmailDomains) > 0 {
-					for _, domain := range config.Config.AllowedEmailDomains {
-						if strings.HasSuffix(claims.User.Email, domain) {
-							// TODO save to db and add roles and stuff
-							return claims
-						}
-					}
-					return claims // don't save to db
-				}
-			}
-			return claims // don't save to db
-		}),
-	}
-
-	// create auth service with google provider
-	authService := auth.NewService(authOptions)
-
-	gClient := auth.Client{
-		Cid:     config.Config.OAuth2.ClientID,
-		Csecret: config.Config.OAuth2.ClientSecret,
-	}
-
-	authService.AddCustomProvider("google", gClient, provider.CustomHandlerOpt{
-		Endpoint: oauth2.Endpoint{
-			AuthURL:  "https://accounts.google.com/o/oauth2/v2/auth",
-			TokenURL: "https://oauth2.googleapis.com/token",
-		},
-		InfoURL: "https://www.googleapis.com/oauth2/v3/userinfo",
-		MapUserFn: func(data provider.UserData, _ []byte) token.User {
-			userInfo := token.User{
-				ID: "google_" + token.HashID(sha1.New(),
-					data.Value("sub")),
-				Name:    data.Value("name"),
-				Email:   data.Value("email"),
-				Picture: data.Value("picture"),
-			}
-			// fail if email is not in AllowedEmailDomains
-			if len(config.Config.AllowedEmailDomains) > 0 {
-				for _, domain := range config.Config.AllowedEmailDomains {
-					if strings.HasSuffix(userInfo.Email, domain) {
-						return userInfo
-					}
-				}
-				return token.User{}
-			}
-			return userInfo
-		},
-		Scopes: []string{"profile", "email"},
-	})
-
-	m := authService.Middleware()
-
 	// setup http server
 	router := chi.NewRouter()
 	router.Use(
 		middleware.Logger,
 	)
-	router.Get("/open", handler.PublicRoute)                    // open api
-	router.With(m.Auth).Get("/private", handler.ProtectedRoute) // protected api
 
 	// setup auth routes
-	authRoutes, avaRoutes := authService.Handlers()
-	router.Mount("/auth", authRoutes)  // add auth handlers
-	router.Mount("/avatar", avaRoutes) // add avatar handler
+	router.Mount("/auth", rtr.AuthRoutes())
+
+	// setup user routes
+	router.Mount("/users", rtr.UserRoutes())
 
 	c := cors.New(cors.Options{
 		AllowedOrigins:   config.Config.AllowedOrigins,
