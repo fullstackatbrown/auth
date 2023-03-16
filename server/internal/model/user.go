3d7ad127f211349384b7db3a5654c7bf68fa58a4
Tianren Dong <tianrendong.dtr@gmail.com>
2023-03-16 14:58:52 +0000

    try new google id

--- server/internal/model/user.go
+++ server/internal/model/user.go
@@ -5,6 +5,7 @@ import "github.com/kamva/mgm/v3"
 type User struct {
 	// DefaultModel adds _id, created_at and updated_at fields to the Model.
 	mgm.DefaultModel `bson:",inline"`
+	GoogleId         string  `json:"googleId" bson:"googleId"`
 	Profile          Profile `json:"profile" bson:"profile"`
 	Roles            Roles   `json:"roles,omitempty" bson:"roles,omitempty"`
 }
@@ -17,8 +18,9 @@ type Profile struct {
 
 type Roles []string
 
-func NewUser(email string) *User {
+func NewUser(googleId string, email string) *User {
 	return &User{
+		GoogleId: googleId,
 		Profile: Profile{
 			Email: email,
 		},
