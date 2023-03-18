f9b21f06c92ab15a5e86c6efae42431bc424fd02
Charles Nguyen <90732040+charlesnguyen99@users.noreply.github.com>
2023-03-18 19:13:08 +0000

    find user and delete user

--- server/internal/db/user.go
+++ server/internal/db/user.go
@@ -13,12 +13,22 @@ func CreateUser(user *model.User) (err error) {
 
 func FindUserById(id string) (user *model.User, err error) {
 	user = &model.User{}
-	err = mgm.Coll(user).FindByID(id, user)
+	err = mgm.Coll(user).FindOne(mgm.Ctx(), bson.M{"googleId" : id}).Decode(user)
 	return
 }
 
 func FindUsersByEmail(email string) (users []model.User, err error) {
 	users = []model.User{}
 	err = mgm.Coll(&model.User{}).SimpleFind(&users, bson.M{"profile.email": email})
+	return
+}
+
+func DeleteUser(userId string) (err error) {
+	// find this user first
+	user, err := FindUserById(userId)
+	if err != nil {
+		return err
+	}
+	err = mgm.Coll(&model.User{}).Delete(user)
 	return
 }
