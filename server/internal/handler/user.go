e4d506b8defebc907b8fd3e6090c4bc2087f494a
Tianren Dong <tianrendong.dtr@gmail.com>
2023-04-05 21:30:14 +0000

    feat: list user roles handler

--- server/internal/handler/user.go
+++ server/internal/handler/user.go
@@ -4,7 +4,9 @@ import (
 	"net/http"
 
 	"github.com/fullstackatbrown/auth-infrastructure/internal/db"
+	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/render"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 func GetUsersByEmail(w http.ResponseWriter, r *http.Request) {
@@ -27,6 +29,26 @@ func GetUserProfile(w http.ResponseWriter, r *http.Request) {
 }
 
 func ListUserRoles(w http.ResponseWriter, r *http.Request) {
+	// get user id from path param
+	userId := chi.URLParam(r, "userId")
+
+	// get user object from db
+	user, err := db.FindUserById(userId)
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			render.Status(r, http.StatusNotFound)
+			render.JSON(w, r, map[string]string{"message": "user not found"})
+			return
+		}
+		render.Status(r, http.StatusInternalServerError)
+		render.JSON(w, r, map[string]string{"message": "internal server error"})
+		return
+	}
+
+	// get roles from user object
+	roles := user.Roles
+
+	render.JSON(w, r, roles)
 }
 
 func AddUserRole(w http.ResponseWriter, r *http.Request) {
